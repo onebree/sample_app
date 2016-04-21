821a43543c4bfa9e090900b795dcd9b515422c89
Hunter Stevens <onebree@gmail.com>
2016-04-21 10:59:49 -0400

    Add named routes and test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root "static_pages#home"
-  get 'static_pages/help'
-  get 'static_pages/about'
-
+  root             "static_pages#home"
+  get "help"    => "static_pages#help"
+  get "about"   => "static_pages#about"
+  get "contact" => "static_pages#contact"
 end
