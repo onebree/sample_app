28d10ee38b69624407da1ee3d5f4c9ffd9d6bd36
Hunter Stevens <onebree@gmail.com>
2016-05-01 17:02:18 -0400

    Use bang-method in before_save (Ex 6.2)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
 
   validates :name,  presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
