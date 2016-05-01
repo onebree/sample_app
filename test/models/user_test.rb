b4340e9e930ba400a6cb3e0f62999330696edf39
Hunter Stevens <onebree@gmail.com>
2016-04-30 21:03:40 -0400

    User now has secure password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(
+      name: "Example User",
+      email: "user@example.com",
+      password: "foobar",
+      password_confirmation: "foobar"
+    )
   end
 
   test "should be valid" do
