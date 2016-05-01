339b68c7d28e5d14d78ba57dbec4275cebce6842
Hunter Stevens <onebree@gmail.com>
2016-05-01 16:17:25 -0400

    Minimum length for password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -68,4 +68,14 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_not duplicate_user.valid?
   end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
 end
