f8c395f5ae1fab69a199ea5ad671ed19c931075b
Hunter Stevens <onebree@gmail.com>
2016-05-02 15:54:22 -0400

    Test edge cases for browser auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,8 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  def authenticated?
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
