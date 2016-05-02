cdab27a53e969b2041339adc41624cd29b068cee
Hunter Stevens <onebree@gmail.com>
2016-05-02 14:44:55 -0400

    User logs in after signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,10 @@ class User < ActiveRecord::Base
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
