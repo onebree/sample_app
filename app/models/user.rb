02f3fdc258dbdbe7003ec87b9c7edd48f6a1fec5
Hunter Stevens <onebree@gmail.com>
2016-05-16 12:03:10 -0400

    Activation tests and refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,6 +39,15 @@ class User < ActiveRecord::Base
     BCrypt::Password.new(digest).is_password?(token)
   end
 
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   private
   
   def downcase_email
