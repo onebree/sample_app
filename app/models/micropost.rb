d2041ae83413c81565c9226b692c246ccd722f6a
Hunter Stevens <onebree@gmail.com>
2016-05-16 14:59:56 -0400

    Associate microposts through users

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,8 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
+
+  default_scope -> { order("created_at DESC") }
+
+  validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
 
   attr_accessor :remember_token, :activation_token, :reset_token
 
+  has_many :microposts, dependent: :destroy
+
   before_save   :downcase_email
   before_create :create_activation_digest
 
