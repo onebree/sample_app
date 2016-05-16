d6cf03a2e33d963f93b2da9019fe1640656b33a9
Hunter Stevens <onebree@gmail.com>
2016-05-16 11:05:01 -0400

    Add activation to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
-  before_save { self.email = email.downcase }
+  before_save   :downcase_email
+  before_create :create_activation_digest
 
   validates :name,  presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
@@ -35,5 +36,16 @@ class User < ActiveRecord::Base
   def authenticated?(remember_token)
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  private
+  
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,9 @@ User.create!(
   email:    "example@railstutorial.org",
   password: "foobar",
   password_confirmation: "foobar",
-  admin:    true
+  admin:     true,
+  activated: true,
+  activated_at: Time.zone.now
 )
 
 99.times do |n|
@@ -14,6 +16,8 @@ User.create!(
     name:     name,
     email:    email,
     password: password,
-    password_confirmation: password
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now
   )
 end
