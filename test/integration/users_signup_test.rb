3a50b7d53fcec324e14226df6f85a434fce41564
Hunter Stevens <onebree@gmail.com>
2016-05-16 12:49:22 -0400

    Password reset tests

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -34,19 +34,22 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
     user = assigns(:user)
     assert_not user.activated?
-
+    
+    # Try to log in before activation.
     log_in_as(user)
     assert_not is_logged_in?
     
+    # Invalid activation token
     get edit_account_activation_path("invalid token")
     assert_not is_logged_in?
-    
+   
+    # Valid token, wrong email
     get edit_account_activation_path(user.activation_token, email: "wrong")
     assert_not is_logged_in?
    
+    # Valid activation token
     get edit_account_activation_path(user.activation_token, email: user.email)
     assert user.reload.activated?
-    
     follow_redirect!
     assert_template "users/show"
     assert is_logged_in?
