270752aa808b386363039f315ac135d812b397c3
Hunter Stevens <onebree@gmail.com>
2016-05-17 09:13:51 -0400

    Image validations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,4 +7,13 @@ class Micropost < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :picture_size
+
+  private
+
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 end
