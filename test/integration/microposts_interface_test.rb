7aeb1adf6812df2037db0add0a94c8f6bcfd8870
Hunter Stevens <onebree@gmail.com>
2016-05-17 21:03:40 -0400

    Test image uploader for microposts (Ex 11.3)

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -9,6 +9,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select "div.pagination"
+    assert_select 'input[type="file"]'
 
     # Invalid submission
     assert_no_difference "Micropost.count" do
@@ -18,9 +19,12 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
 
     # Valid submission
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload("test/fixtures/rails.png", "image/png")
     assert_difference "Micropost.count", 1 do
-      post microposts_path, micropost: { content: content }
+      post microposts_path, micropost: { content: content, picture: picture }
     end
+    micropost = assigns(:micropost)
+    assert micropost.picture?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
