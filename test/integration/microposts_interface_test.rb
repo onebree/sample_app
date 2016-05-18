dccd7c0ff203642a6bdb0ee6f38a518b2706facb
Hunter Stevens <onebree@gmail.com>
2016-05-17 20:46:27 -0400

    Add test for microposts sidebar (Ex 11.2)

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
-
   def setup
     @user = users(:michael)
   end
@@ -36,5 +35,20 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     # Visit a different user.
     get user_path(users(:archer))
     assert_select "a", text: "delete", count: 0
+  end
+
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.microposts.count} microposts", response.body
+
+    # User with zero microposts
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "A micropost")
+    get root_path
+    assert_match "1 micropost", response.body
   end
 end
