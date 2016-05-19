057243ee87ad0dff0b6fdefeae2110cf61ea61e8
Hunter Stevens <onebree@gmail.com>
2016-05-18 21:38:29 -0400

    Relationship model, active_relationships table

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   attr_accessor :remember_token, :activation_token, :reset_token
 
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
 
   before_save   :downcase_email
   before_create :create_activation_digest
