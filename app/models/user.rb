4658cb57b0da1b05707e9604ed976bf7a68731ab
Hunter Stevens <onebree@gmail.com>
2016-05-24 13:06:31 -0400

    Use subselect in feeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -73,7 +73,12 @@ class User < ActiveRecord::Base
   end
 
   def feed
-    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where(
+      "user_id IN (#{following_ids}) OR user_id = :user_id", 
+      user_id: id
+    )
   end
 
   def follow(other_user)
